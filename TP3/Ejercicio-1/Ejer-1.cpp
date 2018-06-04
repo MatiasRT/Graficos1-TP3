0ba133571e53b5a420eea574231b02d1262ff79f
Matias Ruiz Torres <ruiztorresmatias@gmail.com>
2018-06-03 23:15:27 -0300

    TP3-V10
    Menu added to the game
    
    -Add "Menu1" png.
    
    -Add "Menu2" png.

--- TP3/Ejercicio-1/Ejer-1.cpp
+++ TP3/Ejercicio-1/Ejer-1.cpp
@@ -3,17 +3,22 @@
 #include <allegro5\allegro_image.h>
 #include <allegro5\allegro_font.h>
 #include <allegro5\allegro_ttf.h>
-//#include "Button.h"
+#include <allegro5\allegro_native_dialog.h>
 #include "GameplayState.h"
+
 #define WIDTH 650
 #define HEIGHT 750
 
 using namespace std;
 
 int main(int argc, char **argv) {
+	ALLEGRO_KEYBOARD_STATE keyState;
+	ALLEGRO_BITMAP* _background1;
+	ALLEGRO_BITMAP* _background2;
 	ALLEGRO_DISPLAY *display;
-	
 	GameplayState* game;
+	bool gameStart = false;
+	int flicker = 0;
 
 	if (!al_init())
 	{
@@ -50,10 +55,42 @@ int main(int argc, char **argv) {
 	{
 		cerr << "Error initializing Allegro TTF." << endl;
 		return 1;
-	}
+	}	
 	
-	game = new GameplayState(display);
-	game->runGame();
+	
+	_background1 = al_load_bitmap("Assets/Menu1.png");
+	_background2 = al_load_bitmap("Assets/Menu2.png");
+
+
+	while (!gameStart) {
+
+		if (flicker < 1500) {
+			al_draw_bitmap(_background1, 0, 0, 0);
+			flicker++;
+
+		}
+		else if (flicker < 3000){
+			al_draw_bitmap(_background2, 0, 0, 0);
+			flicker++;
+		}
+		else flicker = 0;
+
+
+		al_get_keyboard_state(&keyState);
+		if (al_key_down(&keyState, ALLEGRO_KEY_ENTER))
+			gameStart = true;
+		if (al_key_down(&keyState, ALLEGRO_KEY_ESCAPE))
+			return 0;
+
+		al_flip_display();
+		al_clear_to_color(al_map_rgb(0, 0, 0));
+	}
+
+	if (gameStart) {
+		game = new GameplayState(display);
+		game->runGame();
+	}
+
 
 	al_destroy_display(display);
 	al_uninstall_keyboard();
